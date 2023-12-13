7e3c53db0bda5a71031807165c4442898b19b642
m8dotpie <alentev.olm@ya.ru>
2023-12-13 10:23:44 +0000

    fix: comments

--- varitop/problem.py
+++ varitop/problem.py
@@ -75,17 +75,21 @@ class VaritopProblem:
             raise NotImplementedError()
 
     def get_del_residual(self) -> cs.Function:
-        """Discrete Euler-Lagrange residual
-        D1L + D2L + lambda * phi * dt = 0
-        I am hoping that having lambdas as
-        free variables will allow solution
-        with respect to them and next step"""
+        """Discrete Euler-Lagrange solution
+        D1L(q1, q3) + D2L(q1, q2) + lambda * phi(q2) * dt = 0
+        This equation can be solved to obtain the next state q3
+        """
         discrete_lagrangian = self.get_discrete_lagrangian()
+
+        if self.variables.get(State) is None:
+            raise RuntimeError("State variable not created. Use create_state()")
 
         q1 = cs.SX.sym("q1", self.variables[State].shape[0])
         q2 = cs.SX.sym("q2", self.variables[State].shape[0])
         q3 = cs.SX.sym("q3", self.variables[State].shape[0])
         dt = cs.SX.sym("dt")
+
+        # Lagrange multipliers
         eq_lambdas = cs.SX.sym("eq_lambda", len(self.eq_constraints))
 
         # First slot derivative
@@ -96,13 +100,16 @@ class VaritopProblem:
         d2l = cs.Function(
             "D2L", [q1, q2, dt], [cs.jacobian(discrete_lagrangian(q1, q2, dt), q2)]
         )
-
+        # Initial residual
         del_residual = d1l(q2, q3, dt).T + d2l(q1, q2, dt).T
 
+        # Add the forces of constraints
         for i, phi in enumerate(self.eq_constraints):
             del_residual += cs.jacobian(phi(q2), q2).T * eq_lambdas[i] * dt
 
+        # variable is the vector of unknowns
         variable = cs.vertcat(q3, eq_lambdas)
+
         eq_constraints = cs.vcat([constr(q3) for constr in self.eq_constraints])
         del_residual = cs.vertcat(del_residual, eq_constraints)
 
@@ -117,12 +124,20 @@ class VaritopProblem:
         return del_residual
 
     def get_delm_residual(self) -> [cs.Function, cs.Function]:
+        """Discrete Euler Lagrange in Momentum form solution
+        p_residual = p_prev + D1L(q1, q2) + lambda * phi(q2) * dt
+        p_next = D2L(q1, q2)
+        Residual can be solved to obtain the next state q2
+        p_next provides the momentum on the next state
+        """
         discrete_lagrangian = self.get_discrete_lagrangian()
 
         q1 = cs.SX.sym("q1", self.variables[State].shape[0])
         q2 = cs.SX.sym("q2", self.variables[State].shape[0])
         p = cs.SX.sym("p", self.variables[Momentum].shape[0])
         dt = cs.SX.sym("dt")
+
+        # Lagrange multipliers
         eq_lambdas = cs.SX.sym("eq_lambda", len(self.eq_constraints))
 
         # First slot derivative
@@ -134,11 +149,16 @@ class VaritopProblem:
             "D2L", [q1, q2, dt], [cs.jacobian(discrete_lagrangian(q1, q2, dt), q2)]
         )
 
+        # initial residual
         p_residual = p + d1l(q1, q2, dt).T
+
+        # Add the forces of constraints
         for i, phi in enumerate(self.eq_constraints):
             p_residual += cs.jacobian(phi(q1), q1).T * eq_lambdas[i] * dt
 
+        # variable is the vector of unknowns
         variable = cs.vertcat(q2, eq_lambdas)
+
         eq_constraints = cs.vcat([constr(q2) for constr in self.eq_constraints])
         p_residual = cs.vertcat(p_residual, eq_constraints)
 
@@ -150,4 +170,5 @@ class VaritopProblem:
             ["P_residual"],
         )
 
+        # p_next = D2L(q1, q2)
         return p_residual, d2l
