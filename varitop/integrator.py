fix: quaternions integration

varitop/integrator.py
@@ -1,7 +1,8 @@
 """Integrators"""
 
+from varitop.model import Model
 from typing import List
-from .misc import skew_quaternion
+from .misc import skew_quaternion, qconj
 import casadi as cs
 import numpy as np
 
@@ -10,14 +11,76 @@ class VariationalIntegrator:
     """Abstract variational integrator class"""
 
     def __init__(self) -> None:
+        self._model: Model = None
         self._lagrangian: cs.Function = None
         self._rule: cs.Function = None
         self._nq: int = None
         self._nu: int = None
         self._constrained: bool = False
         self._forced: bool = False
+        self._quaternion_index: int = None
         self._dynamics_constraint: cs.Function = None
         self._generalized_force: cs.Function = None
+
+    @property
+    def quaternion_index(self) -> int or None:
+        """Index of quaternion in generalized coordinates"""
+        return self._quaternion_index
+
+    @quaternion_index.setter
+    def quaternion_index(self, index: int):
+        self._quaternion_index = index
+
+    def v(self, q: cs.SX, dq: cs.SX) -> cs.SX:
+        if self._quaternion_index is None:
+            return dq
+        else:
+            qi = self._quaternion_index
+            quat = q[qi : qi + 4]
+            quat_dot = dq[qi : qi + 4]
+            w = self.q2w(quat, quat_dot)  # angular velocity
+            w = w[1:]  # imaginary part, scalar should be zero
+            v = cs.vertcat(dq[:qi], w, dq[qi + 4 :])
+            return v
+
+    def q2w(self, quat: cs.SX, quat_dot: cs.SX) -> cs.SX:
+        """Angular velocity from quaternion and its derivative
+
+        :math:`\\omega = 2 L(\\bar{q}) \\dot{q}`
+
+        :param quat: quaternion
+        :type quat: casadi.SX
+        :param quat_dot: quaternion derivative
+        :type quat_dot: casadi.SX
+        :return: angular velocity 4x1
+        :rtype: casadi.SX
+        """
+        return 2 * skew_quaternion(qconj(quat)) @ quat_dot
+
+    @property
+    def model(self) -> Model:
+        """Model"""
+        if self._model is None:
+            raise RuntimeError("Model not set.")
+        return self._model
+
+    def create_data(self):
+        """Create data for the model"""
+        self.model.create_data()
+        self.nq = self.model.nq
+        self.nu = self.model.nu
+
+        q = cs.SX.sym("q", self.nq)
+        dq = cs.SX.sym("dq", self.nq)
+        v = self.v(q, dq)
+        self.lagrangian = cs.Function(
+            "L", [q, dq], [self.model.lagrangian(q, v)], ["q", "dq"], ["L"]
+        )
+
+    @model.setter
+    def model(self, model: Model):
+        self._model = model
+        self.create_data()
 
     @property
     def lagrangian(self) -> cs.Function:
@@ -250,23 +313,34 @@ class DelIntegrator(VariationalIntegrator):
         """Reformulate residual to match rootfinder signature"""
         res = self.get_residual()
         nq = self.nq
-        nv = self.nv
         nu = self.nu
 
         q0 = cs.SX.sym("q", nq)
         q1 = cs.SX.sym("q", nq)
         q2 = cs.SX.sym("q", nq)
         dt = cs.SX.sym("dt")
-        lambdas = cs.SX.sym("lambda", 1)
-        u = cs.SX.sym("u", nu)
 
-        x = cs.vertcat(q2, lambdas)
-        rfr = cs.Function("rfr", [x, q0, q1, dt, u], [res(q0, q1, q2, dt, lambdas, u)])
+        x = q2
+        variables = [x, q0, q1, dt]
+        arguments = [q0, q1, q2, dt]
+
+        if self._constrained:
+            lambdas = cs.SX.sym("lambda", self._dynamics_constraint.nnz_out())
+            arguments.append(lambdas)
+            x = cs.vertcat(q2, lambdas)
+            variables[0] = x
+
+        if self._forced:
+            u = cs.SX.sym("u", nu)
+            arguments.append(u)
+            variables.append(u)
+
+        rfr = cs.Function("rfr", variables, [res(*arguments)])
         rf = cs.rootfinder("rf", "newton", rfr)
         return rf
 
     def step(
-        self, q0: np.ndarray, q1: np.ndarray, dt: float, u: np.ndarray
+        self, q0: np.ndarray, q1: np.ndarray, dt: float, u: np.ndarray = None
     ) -> np.ndarray:
         """
         :param q0: :math:`q_{k-1}`
@@ -284,13 +358,20 @@ class DelIntegrator(VariationalIntegrator):
         DEL Residual evolution described by: :math:`(q_0, q_1) \\rightarrow (q_1, q_2)`
         """
         rf = self.get_rf_residual()
+        lambda_len = 0
         guess = q1
 
         if self._constrained:
             # Guess lambdas if any
-            guess = np.hstack([guess, np.zeros(self._dynamics_constraint.nnz_out())])
+            lambda_len = self._dynamics_constraint.nnz_out()
+            guess = np.hstack([guess, np.zeros(lambda_len)])
 
-        sol = np.array(rf(guess, q0, q1, dt, u))[:4].ravel()
+        arguments = [guess, q0, q1, dt]
+
+        if self._forced:
+            arguments.append(u)
+
+        sol = np.array(rf(*arguments))[: len(guess) - lambda_len].ravel()
 
         return sol
 